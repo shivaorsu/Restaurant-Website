de5aa3866f2eb5b0001415f57a7f986feab30abc
shivaorsu <shivaorsu785@gmail.com>
2022-11-25 21:17:57 +0530

    added git hub

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,18 @@
+import {Fragment} from "react";
+import mealsImage from '../../assetes/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from "./HeaderCarButton";
+
+const Header=props=>{
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Hyderabadi Meals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+            <img  src={mealsImage} alt='A table full of delicious food'/>
+        </div>
+    </Fragment>
+
+}
+export default Header
