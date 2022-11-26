8d53f366eae6b639cebfa65882a9418f4be24013
shivaorsu <shivaorsu785@gmail.com>
2022-11-26 22:04:29 +0530

    added and updated

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,20 +1,19 @@
+
 import classes from './MealItem.module.css';
 
+const MealItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
 
-const MealItem =props=>{
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
 
-    const price=`Rs ${props.price.toFixed()}`;
-
-    return (
-        <li className={classes.name}>
-            <div>
-                <h3>{props.name}</h3>
-                <div className={classes.description}>{props.description}</div>
-                <div className={classes.price}>{price}</div>
-            </div>
-            <div></div>
-        </li>
-    )
-
-}
 export default MealItem;
