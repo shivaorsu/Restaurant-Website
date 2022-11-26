fa83756533747f7febffce3627eea03edc3012eb
shivaorsu <shivaorsu785@gmail.com>
2022-11-26 20:52:53 +0530

    added and update

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -1,39 +1,52 @@
+ 
+ import Card from '../UI/Card';
+
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
       id: 'm1',
       name: 'Mirchi Ka salan',
       description: 'comes from coconut',
-      price: 22.99,
+      price: 300,
     },
     {
       id: 'm2',
       name: 'Hyderabadi Biryani',
       description: 'Veg and Non-Veg',
-      price: 16.5,
+      price: 120,
     },
     {
       id: 'm3',
       name: 'Haleem',
       description: 'dish is made with lentils',
-      price: 12.99,
+      price: 100,
     },
     {
       id: 'm4',
       name: 'Hyderabadi Khichdi',
       description: 'Spicy in taste',
-      price: 18.99,
+      price: 180,
     },
   ];
 
 const AvailableMeals=()=>{
-    const mealsList=DUMMY_MEALS.map((meal) => <li>{meal.name}</li>)
+    const mealsList=DUMMY_MEALS.map((meal) =>( 
+       <MealItem 
+         key={meal.id} 
+         name={meal.name} 
+         description={meal.description} 
+         price={meal.price}
+        />
+      )); 
 
     return <section className={classes.meals}>
+      <Card>
         <ul>
             {mealsList}
         </ul>
+     </Card> 
     </section>
 
 
